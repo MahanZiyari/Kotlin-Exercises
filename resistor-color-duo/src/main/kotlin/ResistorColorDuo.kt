012e6f5b42c9a58a210d04cba24b2e9c9b833676
Mahan Ziyari <maziyar.mz.021@gmail.com>
2020-12-22 10:06:04 +0330

    Solved

--- resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
+++ resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
@@ -1,6 +1,8 @@
 object ResistorColorDuo {
 
     fun value(vararg colors: Color): Int {
-        TODO("Implement this to complete the task")
+        val firstColor = Color.valueOf(colors[0].name).ordinal
+        val secondColor = Color.valueOf(colors[1].name).ordinal
+        return  "$firstColor$secondColor".toInt()
     }
 }
