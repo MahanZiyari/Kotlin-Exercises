65f64b0c192b2aafe386de649051f89183987cbc
Mahan Ziyari <maziyar.mz.021@gmail.com>
2020-12-22 10:27:21 +0330

    Adding alternative solutions

--- resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
+++ resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
@@ -1,8 +1,17 @@
 object ResistorColorDuo {
 
     fun value(vararg colors: Color): Int {
-        val firstColor = Color.valueOf(colors[0].name).ordinal
-        val secondColor = Color.valueOf(colors[1].name).ordinal
-        return  "$firstColor$secondColor".toInt()
+        //My solution
+        /**
+         * val firstColor = colors[0].ordinal
+         * val secondColor = colors[1].ordinal
+         * return  "$firstColor$secondColor".toInt()
+         */
+
+        //Alternative solution 1
+        //return "${colors[0].ordinal}${colors[1].ordinal}".toInt()
+
+        //Alternative solution 2
+        return (colors[0].ordinal * 10) + colors[1].ordinal
     }
 }
